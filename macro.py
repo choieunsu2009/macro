55e030dade42e211e0bc7e7f7ca72f909494149b
choieunsoo <109530772+choieunsu2009@users.noreply.github.com>
2025-02-22 00:21:44 +0900

    Create macro.py

---
+++ macro.py
@@ -0,0 +1,22 @@
+import pyautogui
+
+# 지정 위치 확인용 코드
+# while 1:
+#     print(pyautogui.position())
+
+# 변수지정
+loop = int(input("반복 횟수는? >>> "))
+temp = 0
+times = []
+
+# 시간 간격 지정
+for i in loop:
+    temp += int(input("분? >>> ")) * 60
+    temp += int(input("초? >>> "))
+    times.append(temp)
+    temp = 0
+
+# 실행
+for i in range(0, loop):
+    print(i+1,"번째 작동 중... ",loop+"번 작동 후 정지")
+    pyautogui.click(1718,1209,button= 'left', interval= times[i])
